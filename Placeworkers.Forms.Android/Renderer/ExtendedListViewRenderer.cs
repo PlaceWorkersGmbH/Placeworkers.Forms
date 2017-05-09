ba577123434d99c4300ce867459d6db6837f6e0c
Enrico Berndt <enrico.berndt@gmail.com>
2017-05-09 13:54:48 +0200

    Initial Commit

---
+++ Placeworkers.Forms.Android/Renderer/ExtendedListViewRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Placeworkers.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+namespace Placeworkers.Forms
+{
+	public class ExtendedListViewRenderer : ListViewRenderer
+	{
+		public override bool DispatchTouchEvent(Android.Views.MotionEvent e)
+		{
+			if (e.Action == Android.Views.MotionEventActions.Move && !((ExtendedListView)Element).IsScrollable)
+			{
+				return true;
+			}
+			return base.DispatchTouchEvent(e);
+		}
+	}
+}
---
+++ Placeworkers.Forms.iOS/Renderer/ExtendedListViewRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Placeworkers.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+namespace Placeworkers.Forms
+{
+	public class ExtendedListViewRenderer : ListViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged(e);
+			if (Control != null && Element != null)
+			{
+				UpdateScrollable();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case "IsScrollable":
+					UpdateScrollable();
+					break;
+				default:
+					base.OnElementPropertyChanged(sender, e);
+					break;
+			}
+
+		}
+
+		private void UpdateScrollable() { 
+			Control.ScrollEnabled = ((ExtendedListView)Element).IsScrollable;
+		}
+	}
+}
