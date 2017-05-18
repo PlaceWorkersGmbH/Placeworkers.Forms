8b4c9c9b4f11f7087d55216834ab5d725a04f68c
Enrico Berndt <enrico.berndt@gmail.com>
2017-05-18 09:11:54 +0200

    Updated NavigationButtonRenderer

--- Placeworkers.Forms.iOS/Renderer/NavigationButtonRenderer.cs
+++ Placeworkers.Forms.iOS/Renderer/NavigationButtonRenderer.cs
@@ -6,20 +6,28 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(NavigationButton), typeof(NavigationButtonRenderer))]
 namespace Placeworkers.Forms
 {
-	public class NavigationButtonRenderer : ButtonRenderer
-	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
-		{
-			base.OnElementChanged(e);
-			if (Element != null && Control != null)
-			{
-				Control.HorizontalAlignment = UIKit.UIControlContentHorizontalAlignment.Left;
-				Control.VerticalAlignment = UIKit.UIControlContentVerticalAlignment.Center;
-                Control.ContentEdgeInsets = new UIKit.UIEdgeInsets(0, 0, 0, 0);
-                Control.ImageView.TrailingAnchor.ConstraintEqualTo(this.TrailingAnchor, -15).Active = true;
-                Control.ImageView.CenterYAnchor.ConstraintEqualTo(Control.CenterYAnchor, 0).Active = true;
-                Control.ImageView.TranslatesAutoresizingMaskIntoConstraints = false;
-			}
-		}
+    public class NavigationButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Element != null && Control != null)
+            {
+                Control.HorizontalAlignment = UIKit.UIControlContentHorizontalAlignment.Left;
+                Control.VerticalAlignment = UIKit.UIControlContentVerticalAlignment.Center;
+                if (Control.ImageView.Image == null)
+                {
+                    Control.ContentEdgeInsets = new UIKit.UIEdgeInsets(0, 15, 0, 0);
+                }
+                else
+                {
+                    Control.ContentEdgeInsets = new UIKit.UIEdgeInsets(0, 0, 0, 0);
+                    Control.TitleEdgeInsets = new UIKit.UIEdgeInsets(0, 0, 0, 0);
+                    Control.ImageView.TrailingAnchor.ConstraintEqualTo(this.TrailingAnchor, -15).Active = true;
+                    Control.ImageView.CenterYAnchor.ConstraintEqualTo(Control.CenterYAnchor, 0).Active = true;
+                    Control.ImageView.TranslatesAutoresizingMaskIntoConstraints = false;
+                }
+            }
+        }
     }
 }
