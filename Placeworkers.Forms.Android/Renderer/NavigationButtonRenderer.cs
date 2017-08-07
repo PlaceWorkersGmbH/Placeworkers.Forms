0daa1a0e67d6f95d06144f8cc036794debbccf75
Enrico Berndt <berndt@placeworkers.com>
2017-08-07 13:20:17 +0200

    Added one line ellipsis.

--- Placeworkers.Forms.Android/Renderer/NavigationButtonRenderer.cs
+++ Placeworkers.Forms.Android/Renderer/NavigationButtonRenderer.cs
@@ -13,6 +13,8 @@ namespace Placeworkers.Forms.Renderer
             base.OnElementChanged(e);
             if(Element != null && Control != null){
                 var navButton = Element as NavigationButton;
+                Control.Ellipsize = Android.Text.TextUtils.TruncateAt.End;
+                Control.SetSingleLine(true);
                 Control.SetPadding(navButton.TextPaddingLeft, Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
                 Control.Gravity = Android.Views.GravityFlags.Left | Android.Views.GravityFlags.CenterVertical;
                 var compound = Control.GetCompoundDrawables();
